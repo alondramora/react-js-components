b11f340615088ed043c9a042dab9274d4e616174
Alondra Mora <mora_ac@yahoo.com>
2024-10-12 20:04:31 -0700

    first commit

---
+++ react-js-components/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Accordian from './components/accordian'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="accordian-wrapper-on-appjsx">
+       {/* Accordian */}
+       <Accordian/>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
