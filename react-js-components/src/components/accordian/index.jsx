896fa76d604bcf082a673c3a43dbf4c974ab066b
Alondra Mora <mora_ac@yahoo.com>
2024-10-15 18:40:23 -0700

    working accordian with an enable mutli selection button, button is in progress

--- react-js-components/src/components/accordian/index.jsx
+++ react-js-components/src/components/accordian/index.jsx
@@ -1,26 +1,39 @@
 { /* single selection */ }
 { /* multi selection */ }
 import data from "./data"
+import { useState } from "react";
 
 export default function Accordian(){
 
-// const [selected, setSelected] = useState(null);
+const [selected, setSelected] = useState(null);
 
+function handleSingleSelection(getCurrentId){
+	setSelected(getCurrentId === selected ? null : getCurrentId)
+}
+
+console.log(selected)
 	return (
 		<div className="wrapper">
+			<button>Enable Multi Selection</button>
 			<div className="accordian">
 				{
 					data && data.length > 0 ?
 					data.map((dataItem) => (
 						<div className="item">
-							<div className="title">
+							<div onClick={()=>handleSingleSelection(dataItem.id)} className="title">
 								<h3>{dataItem.question}</h3>
 								<span>+</span>
 							</div>
+							{
+								selected === dataItem.id ?
+								<div className="content">{dataItem.answer}</div> 
+								:
+								null
+							}
 						</div>
 					))
 				:
-				<div>No data found</div>
+				(<div>No data found</div>)
 
 				}
 			</div>
